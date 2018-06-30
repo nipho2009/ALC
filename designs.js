3a020390ca05b07523a0a7a95b5489be96541c2e
Nipho2009 <nipho2009@live.com>
2018-06-30 09:02:59 +0200

    Add files via upload
    Added comments on code

--- designs.js
+++ designs.js
@@ -12,30 +12,35 @@ function makeGrid() {
 	event.preventDefault();
     $('tr').remove();
 	
+	//While loop create's the table rows using the height value
     while(i <= height.val()) {
 		
         canvas.append('<tr></tr>');
         row = $('tr').last();
 		
+		//for loop create's the table columns using the width value
         for(var c=1; c<=width.val(); c++) {
             row.append('<td></td>');
         }
         i += 1;
 		
     }
+	//listening for cell click then call the colorSpace function
     $('td').click(colorSpace);
 	
 }
 
+//this function insert and remove color on the table cell's
 function colorSpace(evt) {
 	
     var tableCell = $(evt.target);
 	
+	
     if(tableCell.attr('style')) {
-		
+		//check if the cell already has color set and remove it if it exist
         tableCell.removeAttr('style');
     }else {
-		
+		//insert color on cell
         tableCell.css('background', picker.val());
     }
 }
